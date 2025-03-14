d55fd2f0232fbf1483a9149c8064226d2dbab376
djairb <39774197+djairb@users.noreply.github.com>
2025-03-14 10:13:18 -0300

    up antes dos ajustes

--- client/src/componentes/video/index.jsx
+++ client/src/componentes/video/index.jsx
@@ -1,22 +1,22 @@
 import React, { useEffect, useState } from 'react';
 
 function MainVideo() {
-    
-    
+    useEffect(() => {
+        // Aqui você pode adicionar algum código adicional, caso precise manipular o iframe de forma personalizada.
+    }, []);
 
     return (
-        <section >
+        <section>
             <div className="video-container">
-                    <iframe
-                        src="https://www.youtube.com/embed/DqdZbdetM4s?autoplay=1&mute=1&loop=1&playlist=DqdZbdetM4s"
-                        title="Conheça a Associação Conexão Social"
-                        frameBorder="0"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-                        referrerPolicy="strict-origin-when-cross-origin"
-                        allowFullScreen
-                    ></iframe>
-                </div>
-            
+                <iframe
+                    src="https://www.youtube.com/embed/DqdZbdetM4s?autoplay=1&mute=1&loop=1&playlist=DqdZbdetM4s"
+                    title="Conheça a Associação Conexão Social"
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                    referrerPolicy="strict-origin-when-cross-origin"
+                    allowFullScreen
+                ></iframe>
+            </div>
         </section>
     );
 }
